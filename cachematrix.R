2b97b9d11a1ac604437a58359e73d6e8d31ec23a
Nirmalya Datta <nirmalya.datta@tcs.com>
2017-04-09 12:39:02 +0530

    Add cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+##WEEK 3 Assignment Coursera Data Science R Programming Course - nirmalyadatta1989
 
-## Write a short comment describing this function
+## This function is creating a special "matrix" object which can cache its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { ##define argument with default matrix mode
+  inv <- NULL                               ##initializing inv as null and will holdvalue of matrix inverse in future
+  set <- function(y) {
+    x <<- y                                 ##value of matrix in upper environment that is the parent environment
+    inv <<- NULL
+  }
+  get <- function() x                       ##returns value of matrix argument
+  setinverse <- function(inverse) inv <<- inverse ##assigning inv in parent environment
+  getinverse <- function() inv
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## The following function returns an inverse of the matrix by computing inverse of the special "matrix" returned by "makeCacheMatrix" above
+## It checks if the inverse has been computed earlier for the matrix
+## If yes, it fetches from the cache and does not re compute provided the matrix has not changed
+## If not or if the matrix has changed, it computes the inverse and then sets the value in the cache using setinverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("GETTING CACHED DATA AS INVERSE OF MATRIX WAS CALCULATED EARLIER AND IS PRESENT IN CACHE")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
